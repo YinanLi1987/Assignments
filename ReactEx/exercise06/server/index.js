7181370fe142571e62db8a83c2d772502480bbc2
YinanLi1987 <t1liyi00@students.oamk.fi>
2022-10-10 12:27:02 +0300

    exercise06 done

---
+++ ReactEx/exercise06/server/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2/promise");
+const connection = require("./database");
+
+const app = express();
+app.use(cors());
+// read data posted from front end
+app.use(express.json());
+// read data from url
+app.use(express.urlencoded({ extended: false }));
+
+const port = 3011;
+
+/*app.get("/", async function (req, res) {
+  config.connect(function (err) {
+    if (err) throw err;
+    res.send("finally connected");
+  });
+});*/
+
+app.get("/", async function (req, res) {
+  let sql = "SELECT * FROM task";
+  connection.query(sql, function (err, result) {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.listen(port, function (err) {
+  console.log("listening......." + port);
+});
